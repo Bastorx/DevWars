a4358ce8ec024df0d2783115f04751ce77ba22ad
Bastien Chevallier <cheval_b@etna-alternance.net>
2016-06-29 15:53:12 +0200

    MAJ master

--- components/DashBoard/User.jsx
+++ components/DashBoard/User.jsx
@@ -9,7 +9,12 @@ var User = React.createClass({
 				<h2>MY PROFILE</h2>
 				<h3>Bastorx</h3>
 				<img src="images/bastorx.jpg"/>
-				<p>Email: trololol@youporn.com</p>
+				<div className="user-info">
+					<p>Email: trololol@test.com</p>
+					<p>Elo: 1743</p>
+					<p>Rank: 4232</p>
+					<p>Ratio (Win / Lost): 52%</p>
+				</div>
 			</section>
 		);
 	}
