e856c87320056055ac9ced78ad2a753349cad00a
Bastien Chevallier <cheval_b@etna-alternance.net>
2016-09-05 09:41:32 +0200

    wip

--- actions/ServerActions.js
+++ actions/ServerActions.js
@@ -8,7 +8,7 @@ module.exports = {
     initialize: function (context, url) {
       return q.all([
         context.dispatch(Actions.RECEIVE_TOKEN, context.getCookie("token")),
-        context.dispatch(Actions.RECEIVE_ME, JSON.parse(context.getCookie("me")))
+        context.dispatch(Actions.RECEIVE_ME, context.getCookie("me"))
       ]).then(function() {
         	return context.executeAction(NavigationActions.navigate, { url: url });
       });
